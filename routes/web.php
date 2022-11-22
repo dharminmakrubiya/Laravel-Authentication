9f6948a4252eccaea39a1e5b7f79fc575a910f9a
dharmin <dharminmakrubiya@hotmail.com>
2022-11-22 14:07:20 +0530

    Laravel/UI Package Authentication Done

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+//Laravel/UI Package Authentication
+// Auth::routes();
+
+
+
+
+
+
+
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
